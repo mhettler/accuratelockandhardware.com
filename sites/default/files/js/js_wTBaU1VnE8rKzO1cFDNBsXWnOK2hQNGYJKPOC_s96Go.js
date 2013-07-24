a73cb0918440131faa4cc2237c7400e95331d8f6
Mathew Suen <mathewsuen@gmail.com>
2013-07-24 14:44:44 -0400

    Added blockquote style

---
+++ sites/default/files/js/js_wTBaU1VnE8rKzO1cFDNBsXWnOK2hQNGYJKPOC_s96Go.js
@@ -0,0 +1,185 @@
+/*!
+ * imagesLoaded PACKAGED v3.0.2
+ * JavaScript is all like "You images are done yet or what?"
+ */
+
+(function(e){"use strict";function t(){}function n(e,t){if(r)return t.indexOf(e);for(var n=t.length;n--;)if(t[n]===e)return n;return-1}var i=t.prototype,r=Array.prototype.indexOf?!0:!1;i._getEvents=function(){return this._events||(this._events={})},i.getListeners=function(e){var t,n,i=this._getEvents();if("object"==typeof e){t={};for(n in i)i.hasOwnProperty(n)&&e.test(n)&&(t[n]=i[n])}else t=i[e]||(i[e]=[]);return t},i.getListenersAsObject=function(e){var t,n=this.getListeners(e);return n instanceof Array&&(t={},t[e]=n),t||n},i.addListener=function(e,t){var i,r=this.getListenersAsObject(e);for(i in r)r.hasOwnProperty(i)&&-1===n(t,r[i])&&r[i].push(t);return this},i.on=i.addListener,i.defineEvent=function(e){return this.getListeners(e),this},i.defineEvents=function(e){for(var t=0;e.length>t;t+=1)this.defineEvent(e[t]);return this},i.removeListener=function(e,t){var i,r,s=this.getListenersAsObject(e);for(r in s)s.hasOwnProperty(r)&&(i=n(t,s[r]),-1!==i&&s[r].splice(i,1));return this},i.off=i.removeListener,i.addListeners=function(e,t){return this.manipulateListeners(!1,e,t)},i.removeListeners=function(e,t){return this.manipulateListeners(!0,e,t)},i.manipulateListeners=function(e,t,n){var i,r,s=e?this.removeListener:this.addListener,o=e?this.removeListeners:this.addListeners;if("object"!=typeof t||t instanceof RegExp)for(i=n.length;i--;)s.call(this,t,n[i]);else for(i in t)t.hasOwnProperty(i)&&(r=t[i])&&("function"==typeof r?s.call(this,i,r):o.call(this,i,r));return this},i.removeEvent=function(e){var t,n=typeof e,i=this._getEvents();if("string"===n)delete i[e];else if("object"===n)for(t in i)i.hasOwnProperty(t)&&e.test(t)&&delete i[t];else delete this._events;return this},i.emitEvent=function(e,t){var n,i,r,s=this.getListenersAsObject(e);for(i in s)if(s.hasOwnProperty(i))for(n=s[i].length;n--;)r=t?s[i][n].apply(null,t):s[i][n](),r===!0&&this.removeListener(e,s[i][n]);return this},i.trigger=i.emitEvent,i.emit=function(e){var t=Array.prototype.slice.call(arguments,1);return this.emitEvent(e,t)},"function"==typeof define&&define.amd?define(function(){return t}):e.EventEmitter=t})(this),function(e){"use strict";var t=document.documentElement,n=function(){};t.addEventListener?n=function(e,t,n){e.addEventListener(t,n,!1)}:t.attachEvent&&(n=function(t,n,i){t[n+i]=i.handleEvent?function(){var t=e.event;t.target=t.target||t.srcElement,i.handleEvent.call(i,t)}:function(){var n=e.event;n.target=n.target||n.srcElement,i.call(t,n)},t.attachEvent("on"+n,t[n+i])});var i=function(){};t.removeEventListener?i=function(e,t,n){e.removeEventListener(t,n,!1)}:t.detachEvent&&(i=function(e,t,n){e.detachEvent("on"+t,e[t+n]);try{delete e[t+n]}catch(i){e[t+n]=void 0}});var r={bind:n,unbind:i};"function"==typeof define&&define.amd?define(r):e.eventie=r}(this),function(e){"use strict";function t(e,t){for(var n in t)e[n]=t[n];return e}function n(e){return"[object Array]"===a.call(e)}function i(e){var t=[];if(n(e))t=e;else if("number"==typeof e.length)for(var i=0,r=e.length;r>i;i++)t.push(e[i]);else t.push(e);return t}function r(e,n){function r(e,n,o){if(!(this instanceof r))return new r(e,n);"string"==typeof e&&(e=document.querySelectorAll(e)),this.elements=i(e),this.options=t({},this.options),"function"==typeof n?o=n:t(this.options,n),o&&this.on("always",o),this.getImages(),s&&(this.jqDeferred=new s.Deferred);var h=this;setTimeout(function(){h.check()})}function a(e){this.img=e}r.prototype=new e,r.prototype.options={},r.prototype.getImages=function(){this.images=[];for(var e=0,t=this.elements.length;t>e;e++){var n=this.elements[e];"IMG"===n.nodeName&&this.addImage(n);for(var i=n.querySelectorAll("img"),r=0,s=i.length;s>r;r++){var o=i[r];this.addImage(o)}}},r.prototype.addImage=function(e){var t=new a(e);this.images.push(t)},r.prototype.check=function(){function e(e,r){return t.options.debug&&h&&o.log("confirm",e,r),t.progress(e),n++,n===i&&t.complete(),!0}var t=this,n=0,i=this.images.length;if(this.hasAnyBroken=!1,!i)return this.complete(),void 0;for(var r=0;i>r;r++){var s=this.images[r];s.on("confirm",e),s.check()}},r.prototype.progress=function(e){this.hasAnyBroken=this.hasAnyBroken||!e.isLoaded,this.emit("progress",this,e),this.jqDeferred&&this.jqDeferred.notify(this,e)},r.prototype.complete=function(){var e=this.hasAnyBroken?"fail":"done";if(this.isComplete=!0,this.emit(e,this),this.emit("always",this),this.jqDeferred){var t=this.hasAnyBroken?"reject":"resolve";this.jqDeferred[t](this)}},s&&(s.fn.imagesLoaded=function(e,t){var n=new r(this,e,t);return n.jqDeferred.promise(s(this))});var f={};return a.prototype=new e,a.prototype.check=function(){var e=f[this.img.src];if(e)return this.useCached(e),void 0;if(f[this.img.src]=this,this.img.complete&&void 0!==this.img.naturalWidth)return this.confirm(0!==this.img.naturalWidth,"naturalWidth"),void 0;var t=this.proxyImage=new Image;n.bind(t,"load",this),n.bind(t,"error",this),t.src=this.img.src},a.prototype.useCached=function(e){if(e.isConfirmed)this.confirm(e.isLoaded,"cached was confirmed");else{var t=this;e.on("confirm",function(e){return t.confirm(e.isLoaded,"cache emitted confirmed"),!0})}},a.prototype.confirm=function(e,t){this.isConfirmed=!0,this.isLoaded=e,this.emit("confirm",this,t)},a.prototype.handleEvent=function(e){var t="on"+e.type;this[t]&&this[t](e)},a.prototype.onload=function(){this.confirm(!0,"onload"),this.unbindProxyEvents()},a.prototype.onerror=function(){this.confirm(!1,"onerror"),this.unbindProxyEvents()},a.prototype.unbindProxyEvents=function(){n.unbind(this.proxyImage,"load",this),n.unbind(this.proxyImage,"error",this)},r}var s=e.jQuery,o=e.console,h=o!==void 0,a=Object.prototype.toString;"function"==typeof define&&define.amd?define(["eventEmitter","eventie"],r):e.imagesLoaded=r(e.EventEmitter,e.eventie)}(window);;
+
+
+window.google = window.google || {};
+google.maps = google.maps || {};
+(function() {
+  
+  function getScript(src) {
+    document.write('<' + 'script src="' + src + '"' +
+                   ' type="text/javascript"><' + '/script>');
+  }
+  
+  var modules = google.maps.modules = {};
+  google.maps.__gjsload__ = function(name, text) {
+    modules[name] = text;
+  };
+  
+  google.maps.Load = function(apiLoad) {
+    delete google.maps.Load;
+    apiLoad([0.009999999776482582,[[["https://mts0.googleapis.com/vt?lyrs=m@223000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.googleapis.com/vt?lyrs=m@223000000\u0026src=api\u0026hl=en-US\u0026"],null,null,null,null,"m@223000000"],[["https://khms0.googleapis.com/kh?v=132\u0026hl=en-US\u0026","https://khms1.googleapis.com/kh?v=132\u0026hl=en-US\u0026"],null,null,null,1,"132"],[["https://mts0.googleapis.com/vt?lyrs=h@223000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.googleapis.com/vt?lyrs=h@223000000\u0026src=api\u0026hl=en-US\u0026"],null,null,"imgtp=png32\u0026",null,"h@223000000"],[["https://mts0.googleapis.com/vt?lyrs=t@131,r@223000000\u0026src=api\u0026hl=en-US\u0026","https://mts1.googleapis.com/vt?lyrs=t@131,r@223000000\u0026src=api\u0026hl=en-US\u0026"],null,null,null,null,"t@131,r@223000000"],null,null,[["https://cbks0.googleapis.com/cbk?","https://cbks1.googleapis.com/cbk?"]],[["https://khms0.googleapis.com/kh?v=78\u0026hl=en-US\u0026","https://khms1.googleapis.com/kh?v=78\u0026hl=en-US\u0026"],null,null,null,null,"78"],[["https://mts0.googleapis.com/mapslt?hl=en-US\u0026","https://mts1.googleapis.com/mapslt?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt/ft?hl=en-US\u0026","https://mts1.googleapis.com/mapslt/ft?hl=en-US\u0026"]],[["https://mts0.googleapis.com/vt?hl=en-US\u0026","https://mts1.googleapis.com/vt?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt/loom?hl=en-US\u0026","https://mts1.googleapis.com/mapslt/loom?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt?hl=en-US\u0026","https://mts1.googleapis.com/mapslt?hl=en-US\u0026"]],[["https://mts0.googleapis.com/mapslt/ft?hl=en-US\u0026","https://mts1.googleapis.com/mapslt/ft?hl=en-US\u0026"]]],["en-US","US",null,0,null,null,"https://maps.gstatic.com/mapfiles/","https://csi.gstatic.com","https://maps.googleapis.com","https://maps.googleapis.com"],["https://maps.gstatic.com/intl/en_us/mapfiles/api-3/13/7","3.13.7"],[122914562],1.0,null,null,null,null,0,"",null,null,1,"https://khms.googleapis.com/mz?v=132\u0026",null,"https://earthbuilder.googleapis.com","https://earthbuilder.googleapis.com",null,"https://mts.googleapis.com/vt/icon"], loadScriptTime);
+  };
+  var loadScriptTime = (new Date).getTime();
+  getScript("https://maps.gstatic.com/intl/en_us/mapfiles/api-3/13/7/main.js");
+})();
+;
+(function ($, Drupal, window, document, undefined) {
+
+$(document).ready(function() {
+
+  //mh Contact Map 41.047467,-73.557295
+  
+  function initialize() {
+	  var mapOptions = {
+		zoom: 14,
+		center: new google.maps.LatLng(41.047467, -73.557295),
+		disableDefaultUI: true,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+  		}
+  		var map = new google.maps.Map(document.getElementById('map-canvas'),
+                                mapOptions);
+                                
+              var image = 'madeInStamford.png';
+			  var myLatLng = new google.maps.LatLng(41.047467, -73.557295);
+			  var beachMarker = new google.maps.Marker({
+				  position: myLatLng,
+				  map: map,
+				  title: "Accurate Lock & Hardware"
+			  });
+			  
+			  var styles = [
+			    {
+			      stylers: [
+			        { hue: "#00d4ff" },
+			        { saturation: -80 },
+			        { lightness: -30 }
+			      ]
+			    },{
+			      featureType: "road",
+			      elementType: "geometry",
+			      stylers: [
+			        { lightness: 20 },
+			        { visibility: "simplified" }
+			      ]
+			    },{
+			      featureType: "road",
+			      elementType: "labels",
+			      stylers: [
+			        { visibility: "off" }
+			      ]
+			    }
+			  ];
+			  
+			  map.setOptions({styles: styles});
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+  //ms Carousel Fullscreen hack
+    
+  $('.flexslider ul#flexslider_views_slideshow_flexslider_galleries-block img').each(function(){
+    var imgSrc = $(this).attr('src');
+    console.log(imgSrc);
+    $(this).hide();
+    $(this).parent().css({'background' : 'url(' + imgSrc + ') no-repeat', 'background-size' : 'cover'});
+  });
+  
+  
+  // Product List Previews
+  
+//  $(document).ajaxSuccess(function() {
+  
+    //$('.itemExtra').before('<div class="itemNav"><a href="#" class="moreInfo">View Specs</a><a href="#" class="preview">More Info</a></div>');
+    
+    $('a.preview').click(function() {
+      if ( $(this).text() == 'More Info' ) {
+        $(this).text('Less Info');
+        $(this).parent().parent().find('.moreInfo').show();
+      } else {
+        $(this).text('More Info');
+        $(this).parent().parent().find('.moreInfo').hide();
+      };
+      return false;
+    });
+  
+//  });
+
+  // Duplicate fixed elements to create white overlay version
+  
+  if ($('.fixed > div').hasClass('dup')) {
+    $('.fixed > div').each(function(){
+      $(this).clone().appendTo($(this).parent()).addClass('overlay');
+      $(this).addClass('underlay');
+    });
+    
+      function cropScroll() {
+        var currPosition = Math.abs($(window).scrollTop());
+    
+        var overClipTop = 0 - currPosition;
+        var overClipBottom = overClipTop + $('.splashSection').height();  
+        var overRect = 'rect(' +overClipTop + 'px auto ' + overClipBottom + 'px 0px)';
+        
+        var underClipBottom = currPosition + $(window).height();
+        var underClipTop= overClipBottom;
+        
+        var underRect = 'rect(' + underClipTop + 'px auto ' + underClipBottom + 'px 0px)';
+        
+        $('.overlay').css({clip : overRect});
+        $('.underlay').css({clip : underRect});
+
+      };
+      
+      $(window).load(function () {
+        cropScroll();
+      });
+      
+      $(window).scroll(function () { 
+        cropScroll();
+      });
+      
+      $(window).resize(function () {
+        cropScroll()
+      });
+    
+  };  
+});
+
+
+  // Sticky series titles in lists
+  
+  
+  
+   $(window).scroll(function() {
+     $('.view-content > h3').addClass('stickyHeaders');
+     $('.stickyHeaders').each(function() {
+       
+       var currPos = $(this).offset().top - 32;
+       var winPos = $(window).scrollTop();
+       
+       if ( currPos <= winPos ) {
+         $('.headerFixed > .stickyHeaders').unwrap();
+         $(this).wrap('<div class="headerFixed" />');
+       };
+       
+       if ( ($('.view-content').offset().top) >= winPos ) {
+         $('.headerFixed > .stickyHeaders').unwrap();
+       };
+ 
+     });
+   });
+
+
+//  
+//  $(document).ajaxSuccess(function() {
+//    alert("An individual AJAX call has completed successfully");
+//  });
+
+
+})(jQuery, Drupal, this, this.document);
+
+;
