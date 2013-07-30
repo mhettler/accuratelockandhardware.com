08b7383ee0700591d01a67bd8e75b774a1efb6da
mhettler <mhettler@mhettler.domain>
2013-07-30 18:21:21 -0400

    Tooltips Module

--- sites/all/themes/alhx/templates/page--front.tpl.php
+++ sites/all/themes/alhx/templates/page--front.tpl.php
@@ -96,60 +96,61 @@
   
     <div class="mainNav fixed">
       <div class="dup">
-      <?php if ($main_menu): ?>
-        <nav class="main-menu" role="navigation">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see http://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>    
+      
+    <?php if ($main_menu): ?>
+        
+			<nav class="main-menu" role="navigation">
+			  <?php
+			  // This code snippet is hard to modify. We recommend turning off the
+			  // "Main menu" on your sub-theme's settings form, deleting this PHP
+			  // code block, and, instead, using the "Menu block" module.
+			  // @see http://drupal.org/project/menu_block
+			  print theme('links__system_main_menu', array(
+				'links' => $main_menu,
+				'attributes' => array(
+				  'class' => array('links', 'inline', 'clearfix'),
+				),
+				'heading' => array(
+				  'text' => t('Main menu'),
+				  'level' => 'h2',
+				  'class' => array('element-invisible'),
+				),
+			  )); ?>
+			</nav>    
   
-      <?php if ($site_name || $site_slogan): ?>
-        <hgroup id="name-and-slogan">
-          <?php if ($site_name): ?>
-            <h1 id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
+		  <?php if ($site_name || $site_slogan): ?>
+			<hgroup id="name-and-slogan">
+			  <?php if ($site_name): ?>
+				<h1 id="site-name">
+				  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+				</h1>
+			  <?php endif; ?>
   
-          <?php if ($site_slogan): ?>
-            <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
-          <?php endif; ?>
-        </hgroup><!-- /#name-and-slogan -->
-      <?php endif; ?>
+			  <?php if ($site_slogan): ?>
+				<h2 id="site-slogan"><?php print $site_slogan; ?></h2>
+			  <?php endif; ?>
+			</hgroup><!-- /#name-and-slogan -->
+		  <?php endif; ?>
   
-      <?php if ($secondary_menu): ?>
-        <nav id="secondary-menu" role="navigation">
-          <?php print theme('links__system_secondary_menu', array(
-            'links' => $secondary_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => $secondary_menu_heading,
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
+		  <?php if ($secondary_menu): ?>
+			<nav id="secondary-menu" role="navigation">
+			  <?php print theme('links__system_secondary_menu', array(
+				'links' => $secondary_menu,
+				'attributes' => array(
+				  'class' => array('links', 'inline', 'clearfix'),
+				),
+				'heading' => array(
+				  'text' => $secondary_menu_heading,
+				  'level' => 'h2',
+				  'class' => array('element-invisible'),
+				),
+			  )); ?>
+			</nav>
+    	  <?php endif; ?>
   
-      <?php print render($page['header']); ?>   
 
-    </div>
-  </div>
+    	</div>
+  	</div>
     
   <?php endif; ?>
 
@@ -174,7 +175,7 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
+      <?php //print $feed_icons; ?>
     </div><!-- /#content -->
 
     <?php
